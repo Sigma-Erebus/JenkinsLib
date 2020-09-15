d2ec37a81f0287d94a06b281f4b6422e637368ea
David ten Kate <davidtotal@outlook.com>
2020-09-15 21:55:21 +0200

    Fixed undeclared variable

--- vars/discord.groovy
+++ vars/discord.groovy
@@ -2,29 +2,29 @@ import groovy.json.JsonOutput
 
 def createMessage(title, status, fields, url, content = null){
 
-def color = 16711680
+   def color = 16711680
 
-if ( status == "ok"){
-    color = 65280
-}else if(status == "new"){
-    color = "359360"
-}
+   if ( status == "ok"){
+      color = 65280
+   }else if(status == "new"){
+       color = "359360"
+   }
 
-def  body = [embeds: 
-[[
+   def body = [embeds: 
+   [[
     title: title,
     color: color,
     fields: fields
-]
-]
-]
+    ]]]
+	
     if (url != null){
         body.embeds[0].url = url
     }
     if(content !=null){
         body.content = content
     }
-return JsonOutput.toJson(body).replace('"','""')
+	
+    return JsonOutput.toJson(body).replace('"','""')
 }
 
 def sendMessage(message, webhook)
@@ -39,10 +39,11 @@ def sendMessage(message, webhook)
 
 def succeeded(webhook)
 {
-   sendMessage(discord.createMessage(":white_check_mark: Build #${env.BUILD_NUMBER} - Last Perforce Revision: ${env.P4_CHANGELIST} Passed Building","ok",[
-                    [name:"Last Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true]
-                ],"${CHANGE_URL}${env.P4_CHANGELIST}","**Status: Success**\nBuild URL: ${env.BUILD_URL}")
-                , webhook)
+   sendMessage(discord.createMessage(":white_check_mark: Build #${env.BUILD_NUMBER} - Last Perforce Revision: ${env.P4_CHANGELIST} Passed Building",
+                                     "ok",
+									 [[name:"Last Perforce Revision", value:"${env.P4_CHANGELIST}", inline:true]]
+									 ,"${env.P4_CHANGELIST}","**Status: Success**\nBuild URL: ${env.BUILD_URL}")
+                                 , webhook)
 }
 
 def failed(webhook)
