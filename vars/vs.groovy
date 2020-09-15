6a3641a53605ce45373556ad719486ba60016322
David ten Kate <davidtotal@outlook.com>
2020-09-15 17:26:21 +0200

    Removed double quotes

--- vars/vs.groovy
+++ vars/vs.groovy
@@ -1,4 +1,4 @@
 def build(MSBuildPath, projectPath, config, platform)
 {
-   bat(script: 'CALL \"${MSBuildPath}\" \"${projectPath}\" /t:build /p:Configuration=${config};Platform=${platform};verbosity=diagnostic')
+   bat(script: 'CALL \'${MSBuildPath}\' \'${projectPath}\' /t:build /p:Configuration=${config};Platform=${platform};verbosity=diagnostic')
 }
