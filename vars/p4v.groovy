fb1f8f068ab20c035ee41642dfc0252b93fbde51
David ten Kate <davidtotal@outlook.com>
2020-09-15 15:10:28 +0200

    Fixed mistake in p4v script

--- vars/p4v.groovy
+++ vars/p4v.groovy
@@ -1,11 +1,4 @@
-def sync(credential, workspace, forceCleanws=true)
+def sync(credential, workspace)
 {
-   if (forceCleanws)
-   {
-      p4sync charset: 'none', credential: credential, format: ${JOB_NAME}, populate: forceClean(have: false, parallel: [enable: true, minbytes: '1024', minfiles: '1', threads: '4'], pin: '', quiet: true), source: templateSource(workspace)
-   }
-   else
-   {
-      p4sync charset: 'none', credential: credential, format: ${JOB_NAME}, populate: autoClean(delete: true, modtime: false, parallel: [enable: true, minbytes: '1024', minfiles: '1', threads: '4'], pin: '', quiet: true, replace: true, tidy: true), source: templateSource(workspace)
-   }
+   p4sync charset: 'none', credential: credential, format: 'jenkins-${JOB_NAME}', populate: autoClean(delete: false, modtime: false, parallel: [enable: false, minbytes: '1024', minfiles: '1', threads: '4'], pin: '', quiet: true, replace: true, tidy: false), source: templateSource(workspace)
 }
