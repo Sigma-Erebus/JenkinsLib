be3d844d57799379d2da36c3be23a8aa0b51bc0d
David ten Kate <davidtotal@outlook.com>
2020-09-15 21:30:57 +0200

    imported jsonoutput lib

--- vars/discord.groovy
+++ vars/discord.groovy
@@ -1,16 +1,48 @@
-def sendMessage(webhook)
+import groovy.json.JsonOutput
+
+def createMessage(title, status, fields, url, content = null){
+
+def color = 16711680
+
+if ( status == "ok"){
+    color = 65280
+}else if(status == "new"){
+    color = "359360"
+}
+
+def  body = [embeds: 
+[[
+    title: title,
+    color: color,
+    fields: fields
+]
+]
+]
+    if (url != null){
+        body.embeds[0].url = url
+    }
+    if(content !=null){
+        body.content = content
+    }
+return JsonOutput.toJson(body).replace('"','""')
+}
+
+def sendMessage(message, webhook)
 {
    bat(script: """
       curl -X POST ^
       -H "Content-Type: application/json" ^
-      -d "{\"username\": \"test\", \"content\": \"hello\"}" ^
+      -d \"${message}\" ^
       ${webhook}
    """)
 }
 
 def succeeded(webhook)
 {
-   sendMessage("Build Success", ${webhook})
+   sendMessage(discord.createMessage(":white_check_mark: Build #${env.BUILD_NUMBER} - Last Perforce Revision: ${env.P4_CHANGELIST} Passed Building","ok",[
+                    [name:"Last Perforce Revision",value:"${env.P4_CHANGELIST}",inline:true]
+                ],"${CHANGE_URL}${env.P4_CHANGELIST}","**Status: Success**\nBuild URL: ${env.BUILD_URL}")
+                , webhook))
 }
 
 def failed(webhook)
