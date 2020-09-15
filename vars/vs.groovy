732990a70e5ae472f023fde567481d610370f62f
David ten Kate <davidtotal@outlook.com>
2020-09-15 15:48:28 +0200

    Fixed spelling mistake

--- vars/vs.groovy
+++ vars/vs.groovy
@@ -1,4 +1,4 @@
-def build(MSBuildPath, solution, config, platform)
+def build(MSBuildPath, project, config, platform)
 {
    bat(script: "CALL ${MSBuildPath} '${project}' /t:build /p:Configuration=${config};Platform=${platform};verbosity=diagnostic")
 }
