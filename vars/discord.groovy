64cce6e6333926f38f51a8319207496867322917
David ten Kate <davidtotal@outlook.com>
2020-09-15 22:01:36 +0200

    Added temp url for placeholding

--- vars/discord.groovy
+++ vars/discord.groovy
@@ -42,7 +42,7 @@ def succeeded(webhook)
    sendMessage(createMessage(":white_check_mark: Build #${env.BUILD_NUMBER} - Last Perforce Revision: ${env.P4_CHANGELIST} Passed Building",
                                      "ok",
 									 [[name:"Last Perforce Revision", value:"${env.P4_CHANGELIST}", inline:true]]
-									 ,"${env.P4_CHANGELIST}","**Status: Success**\nBuild URL: ${env.BUILD_URL}")
+									 ,"https://jbs1.buas.nl:8443/job/Y2020-Y3/job/Y2020-Y3-Arid/job/Arid-TestEnv/","**Status: Success**\nBuild URL: ${env.BUILD_URL}")
                                  , webhook)
 }
 
