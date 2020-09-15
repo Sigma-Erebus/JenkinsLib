1d0e90393f69ac12c3b0b4029c32131b14936edd
David ten Kate <davidtotal@outlook.com>
2020-09-15 17:11:14 +0200

    Revert "potential fix for visual studio script"
    This reverts commit 966f98150105be453be9868a061beeb0abe3605f.

--- vars/vs.groovy
+++ vars/vs.groovy
@@ -1,4 +1,4 @@
 def build(MSBuildPath, project, config, platform)
 {
-   bat(script: "CALL "${MSBuildPath}" "${project}" /t:build /p:Configuration=${config};Platform=${platform};verbosity=diagnostic")
+   bat(script: "CALL '${MSBuildPath}' '${project}' /t:build /p:Configuration=${config};Platform=${platform};verbosity=diagnostic")
 }
