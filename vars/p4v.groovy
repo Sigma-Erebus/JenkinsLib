b58851f6d6f15faf5829b86230bd03f712efa1ae
David ten Kate <davidtotal@outlook.com>
2020-09-17 14:26:23 +0200

    Fixed dereferencing variable

--- vars/p4v.groovy
+++ vars/p4v.groovy
@@ -6,8 +6,8 @@ def sync(credential, workspace)
 def createTicket(credential, p4host)
 {
    def ticket = ""
-   withCredentials([usernamePassword(credentialsId: 'credential', passwordVariable: 'P4PASS', usernameVariable: 'P4USER')]) {
-      bat(script: 'FOR /F "tokens=*" %%F IN ("echo %P4PASS%| p4 -p ssl:swarm2.buas.nl:1667 -u %P4USER% login -ap") do SET ${ticket}=%%F')
+   withCredentials([usernamePassword(credentialsId: '${credential}', passwordVariable: 'P4PASS', usernameVariable: 'P4USER')]) {
+      bat(script: 'FOR /F "tokens=*" %%F IN ("echo %P4PASS%| p4 -p ${p4host} -u %P4USER% login -ap") do SET ${ticket}=%%F')
    }
    
    return ticket
