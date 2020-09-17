4ece7bb098c7f96f5ca8a1a1be58c8e9bc90c2f9
David ten Kate <davidtotal@outlook.com>
2020-09-17 14:30:17 +0200

    Fixed small mistake

--- vars/p4v.groovy
+++ vars/p4v.groovy
@@ -6,7 +6,7 @@ def sync(credential, workspace)
 def createTicket(credential, p4host)
 {
    def ticket = ""
-   withCredentials([usernamePassword(credentialsId: ${credential}, passwordVariable: 'P4PASS', usernameVariable: 'P4USER')]) {
+   withCredentials([usernamePassword(credentialsId: credential, passwordVariable: 'P4PASS', usernameVariable: 'P4USER')]) {
       bat(script: 'FOR /F "tokens=*" %%F IN ("echo %P4PASS%| p4 -p ${p4host} -u %P4USER% login -ap") do SET ${ticket}=%%F')
    }
    
