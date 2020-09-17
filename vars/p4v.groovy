43bad235bac265d58150a0671ff58355815d058b
David ten Kate <davidtotal@outlook.com>
2020-09-17 21:01:33 +0200

    Removed helper functions

--- vars/p4v.groovy
+++ vars/p4v.groovy
@@ -15,39 +15,3 @@ def createTicket(credential, p4host)
    
    return ticket
 }
-
-def upVote(review,user,ticket,swarm_url){
-    swarm.init(user,ticket,swarm_url)
-    swarm.upVote(review)
-    swarm.clear()
-}
-
-def withSwarm(credentials, p4Port, client, mapping, Closure body){
-        withSwarmUrl(env.P4USER,env.P4WORKSPACE,env.P4MAPPING)
-        { 
-            url,user->
-            def p4Ticket = createTicket(credentials,p4Port)
-            body(user,p4Ticket,url)
-        }
-}
-
-def withSwarmUrl(credentials,client,mapping,Closure body){
-        withCredentials([usernamePassword(credentialsId: credentials, passwordVariable: 'p4USERPASS', usernameVariable: 'p4USER' )]) {
-            def url = swarmUrl(credentials,client,mapping)
-            body(url,env.p4USER,env.p4USERPASS)
-        }
-}
-
-def swarmUrl(credential,client,mapping){
-    def p4s = p4(credential: credential, workspace: manualSpec(charset: 'none', cleanup: false, name: client, pinHost: false, spec: clientSpec(allwrite: true, backup: true, changeView: '', clobber: false, compress: false, line: 'LOCAL', locked: false, modtime: false, rmdir: false, serverID: '', streamName: '', type: 'WRITABLE', view: mapping)))
-    def prop = p4s.run("property","-l","-n","P4.Swarm.URL")
-    for(def item : prop) {
-        for (String key : item.keySet()) {
-            if(key == "value")
-            {
-                return item.get(key)
-            }
-        }
-    }
-    return ""
-}
