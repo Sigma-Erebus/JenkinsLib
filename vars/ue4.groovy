057b903a119af58bbd4e302d808c8e7efdf8b32f
David ten Kate <davidtotal@outlook.com>
2020-09-21 19:37:13 +0200

    Added unreal engine script

---
+++ vars/ue4.groovy
@@ -0,0 +1,4 @@
+def build(engineRoot, projectPath, config, platform, outputDir)
+{
+   bat(script: "CALL \"${engineDir}Engine\\Build\\BatchFiles\\RunUAT.bat\" BuildCookRun -Project=\"${projectPath}\" -NoP4 -Distribution -TargetPlatform=${platform} -Platform=${platform} -ClientConfig=${config} -ServerConfig=${config} -Cook -Allmaps -Build -Stage -Pak -Archive -Archivedirectory=\"${outputDir}\" -Rocket -Prereqs -Package")
+}
