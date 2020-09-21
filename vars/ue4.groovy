2641c834b4a50eacaf849fd050c6b1e525b12466
David ten Kate <davidtotal@outlook.com>
2020-09-21 20:18:53 +0200

    Fixed syntax error

--- vars/ue4.groovy
+++ vars/ue4.groovy
@@ -1,4 +1,4 @@
 def build(engineRoot, projectPath, config, platform, outputDir)
 {
-   bat(script: "CALL \"${engineDir}Engine\\Build\\BatchFiles\\RunUAT.bat\" BuildCookRun -Project=\"${projectPath}\" -NoP4 -Distribution -TargetPlatform=${platform} -Platform=${platform} -ClientConfig=${config} -ServerConfig=${config} -Cook -Allmaps -Build -Stage -Pak -Archive -Archivedirectory=\"${outputDir}\" -Rocket -Prereqs -Package")
+   bat(script: "CALL \"${engineRoot}Engine\\Build\\BatchFiles\\RunUAT.bat\" BuildCookRun -Project=\"${projectPath}\" -NoP4 -Distribution -TargetPlatform=${platform} -Platform=${platform} -ClientConfig=${config} -ServerConfig=${config} -Cook -Allmaps -Build -Stage -Pak -Archive -Archivedirectory=\"${outputDir}\" -Rocket -Prereqs -Package")
 }
