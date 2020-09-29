0797b1537758c6fa95b1431acb4d1508f517bb8a
David ten Kate <davidtotal@outlook.com>
2020-09-29 11:40:49 +0200

    Removed curl command line string

--- vars/swarm.groovy
+++ vars/swarm.groovy
@@ -15,7 +15,8 @@ def clear()
 
 def createReview(id, participants = null, desc = null)
 {
-   def response = bat(script: "curl -u \"${swarmInfo.user}:${swarmInfo.ticket}\" -X POST -d \"change=${id}\" \"${swarmInfo.url}/api/v9/reviews/\"", returnStdout: true)
+   def output = bat(script: "curl -u \"${swarmInfo.user}:${swarmInfo.ticket}\" -X POST -d \"change=${id}\" \"${swarmInfo.url}/api/v9/reviews/\"", returnStdout: true)
+   def response = output.split('\n').drop(1).join('\n')
    return response
 }
 
