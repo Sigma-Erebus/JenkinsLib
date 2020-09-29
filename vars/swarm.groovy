dbaf953520babc54d875a2c7a23881a699d461ea
David ten Kate <davidtotal@outlook.com>
2020-09-29 11:53:58 +0200

    Returned correct response

--- vars/swarm.groovy
+++ vars/swarm.groovy
@@ -16,8 +16,8 @@ def clear()
 def createReview(id, participants = null, desc = null)
 {
    def output = bat(script: "curl -u \"${swarmInfo.user}:${swarmInfo.ticket}\" -X POST -d \"change=${id}\" \"${swarmInfo.url}/api/v9/reviews/\"", returnStdout: true)
-   def response = output.split('\\n').drop(1).join('\\n')
-   return response
+   def responseArray = output.split()
+   return responseArray[2].trim()
 }
 
 def getReviewID(curlResponse)
