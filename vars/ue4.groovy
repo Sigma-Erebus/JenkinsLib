8c74e08e1a6a1989fa6d155148b77dc834660bed
David ten Kate <davidtotal@outlook.com>
2020-10-06 14:57:40 +0200

    Added function to fix up redirectors

--- vars/ue4.groovy
+++ vars/ue4.groovy
@@ -9,11 +9,16 @@ def build(engineRoot, projectName, project, config, platform, outputDir, bluepri
       bat(script: "\"${engineRoot}Engine\\Binaries\\DotNET\\UnrealBuildTool.exe\" ${projectName} ${config} ${platform} -project=\"${project}\" -rocket -editorrecompile -progress -noubtmakefiles -NoHotReloadFromIDE -2019")
 
       // Package
-      bat(script: "CALL \"${engineRoot}Engine\\Build\\BatchFiles\\RunUAT.bat\" BuildCookRun -Project=\"${project}\" -NoP4 -Distribution -TargetPlatform=${platform} -Platform=${platform} -ClientConfig=${config} -ServerConfig=${config} -Cook -Allmaps -Build -Stage -Pak -Archive -Archivedirectory=\"${outputDir}\" -Rocket -Prereqs -Package")
+      bat(script: "\"${engineRoot}Engine\\Build\\BatchFiles\\RunUAT.bat\" BuildCookRun -Project=\"${project}\" -NoP4 -Distribution -TargetPlatform=${platform} -Platform=${platform} -ClientConfig=${config} -ServerConfig=${config} -Cook -Allmaps -Build -Stage -Pak -Archive -Archivedirectory=\"${outputDir}\" -Rocket -Prereqs -Package")
    }
    else
    {
       // Only package since we have a blueprintOnly project
-      bat(script: "CALL \"${engineRoot}Engine\\Build\\BatchFiles\\RunUAT.bat\" BuildCookRun -Project=\"${project}\" -NoP4 -Distribution -TargetPlatform=${platform} -Platform=${platform} -ClientConfig=${config} -ServerConfig=${config} -Cook -Allmaps -Build -Stage -Pak -Archive -Archivedirectory=\"${outputDir}\" -Rocket -Prereqs -Package")
+      bat(script: "\"${engineRoot}Engine\\Build\\BatchFiles\\RunUAT.bat\" BuildCookRun -Project=\"${project}\" -NoP4 -Distribution -TargetPlatform=${platform} -Platform=${platform} -ClientConfig=${config} -ServerConfig=${config} -Cook -Allmaps -Build -Stage -Pak -Archive -Archivedirectory=\"${outputDir}\" -Rocket -Prereqs -Package")
    }
+}
+
+def fixupredirects(engineRoot, project)
+{
+   bat(script: "\"${engineRoot}Engine\\Binaries\\Win64\\UE4Editor.exe\" \"${project}\" -run=ResavePackages -fixupredirects -autocheckout -projectonly -unattended")
 }
