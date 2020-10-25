aa14661587e8acc23c940adf3ab8903247ba1853
David ten Kate <davidtotal@outlook.com>
2020-10-25 10:24:06 +0100

    Added unpack function

--- vars/zip.groovy
+++ vars/zip.groovy
@@ -2,3 +2,8 @@ def pack(source, archiveName)
 {
    powershell(label: "Pack into zip file", script: "Compress-Archive -Path \"${source}\" -DestinationPath \"${archiveName}.zip\"");
 }
+
+def unpack(archiveName, destination)
+{
+   powershell(label: "Unpack zip file", script: "Expand-Archive -Path \"${archiveName}\" -DestinationPath \"${destination}\"");
+}
