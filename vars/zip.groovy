436c672cb62e1c3c22975ff66274fff1e3ca549e
David ten Kate <davidtotal@outlook.com>
2020-10-26 23:31:07 +0100

    Added 7z support

--- vars/zip.groovy
+++ vars/zip.groovy
@@ -1,9 +1,23 @@
-def pack(source, archiveName)
+def pack(source, archiveName, use7z = true)
 {
-   powershell(label: "Pack into zip file", script: "Compress-Archive -Path \"${source}\" -DestinationPath \"${archiveName}.zip\"");
+   if (use7z)
+   {
+      bat(label: "Pack into zip file using 7z", script: "7z a \"${archiveName}.zip\" \"${source}\\*\"")
+   }
+   else
+   {
+      powershell(label: "Pack into zip file using powershell", script: "Compress-Archive -Path \"${source}\" -DestinationPath \"${archiveName}.zip\"");
+   }
 }
 
-def unpack(archiveName, destination)
+def unpack(archiveName, destination, use7z = true)
 {
-   powershell(label: "Unpack zip file", script: "Expand-Archive -Path \"${archiveName}\" -DestinationPath \"${destination}\"");
+   if (use7z)
+   {
+      bat(label: "Unpack zip file using 7z", script: "7z e \"${archiveName}.zip\" \"${destination}\"")
+   }
+   else
+   {
+      powershell(label: "Unpack zip file using powershell", script: "Expand-Archive -Path \"${archiveName}.zip\" -DestinationPath \"${destination}\"");
+   }
 }
