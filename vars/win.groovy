95a19cee9a7c7522fc521aa99918d850fdb3adee
Hidde Derks <65902131+Sigma-Erebus@users.noreply.github.com>
2023-03-20 11:21:38 +0100

    Add copy and move commands

--- vars/win.groovy
+++ vars/win.groovy
@@ -2,3 +2,8 @@ def makeWritable(folderPath)
 {
    bat(label: "Removing Read-Only flags", script: "attrib -r ${folderPath}*.* /s")
 }
+
+def copyPath(sourcePath, targetPath)
+{
+    bat(label: "Copying files", script: "robocopy ${sourcePath} ${targetPath}")
+}
