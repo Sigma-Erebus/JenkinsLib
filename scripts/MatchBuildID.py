f99d016f0fc034d29861749da48f9dbf2fefa323
Hidde Derks <65902131+Sigma-Erebus@users.noreply.github.com>
2023-04-25 12:54:30 +0200

    Add files via upload

--- scripts/MatchBuildID.py
+++ scripts/MatchBuildID.py
@@ -9,6 +9,11 @@ PROJECTDIR = sys.argv[1]
 # EngineFolder
 ENGINEDIR = sys.argv[2]
 
+# Also update project modules
+INCLUDEPROJECTMODULES = sys.argv[3]
+
+# Get the BuildId from the source engine
+
 SourceBuildIdFile = glob.glob(pathname="Engine/Plugins/Animation/LiveLink/Binaries/Win64/UnrealEditor.modules", root_dir=ENGINEDIR)[0]
 #print(ENGINEDIR+SourceBuildIdFile)
 file = open(ENGINEDIR+SourceBuildIdFile, "rt")
@@ -27,3 +32,12 @@ for i in TargetBuildIdFiles:
     json.dump(data, target)
     target.close()
     
+if INCLUDEPROJECTMODULES.lower() == "true":
+    os.chmod(PROJECTDIR+"Binaries/Win64/UnrealEditor.modules", stat.S_IWRITE)
+    target2 = open(PROJECTDIR+"Binaries/Win64/UnrealEditor.modules", "rt")
+    data2 = json.load(target2)
+    data2['BuildId'] = SourceBuildId
+    target2.close()
+    target2 = open(PROJECTDIR+"Binaries/Win64/UnrealEditor.modules", "wt")
+    json.dump(data2, target2)
+    target2.close()
