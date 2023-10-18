c28ddd04aa4a401b35b40d4a1ac5b4999a35a364
Hidde Derks <65902131+Sigma-Erebus@users.noreply.github.com>
2023-10-18 23:03:31 +0200

    Update p4v.groovy

--- vars/p4v.groovy
+++ vars/p4v.groovy
@@ -19,7 +19,7 @@ def initGetLatestCL(p4credential, p4host)
    p4Info = [credential: p4credential, host: p4host]
    def p4s = p4(credential: p4Info.credential, workspace: manualSpec(charset: 'none', cleanup: false, name: 'Jenkins-${NODE_NAME}', pinHost: false, spec: clientSpec(allwrite: true, backup: true, changeView: '', clobber: true, compress: false, line: 'LOCAL', locked: false, modtime: false, rmdir: false, serverID: '', streamName: '', type: 'WRITABLE', view: '')))
    def change = p4s.run('changes', '-s', 'submitted', '-m1')
-   def changeString = change.tokenize(' ')
+   def changeString = change.tokenize()
    return changeString[1]
 }
 
