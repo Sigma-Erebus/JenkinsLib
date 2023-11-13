47dffa68f8174580e5064bbd82573d230210dcc7
Hidde Derks <65902131+Sigma-Erebus@users.noreply.github.com>
2023-11-13 14:36:57 +0100

    Update win.groovy

--- vars/win.groovy
+++ vars/win.groovy
@@ -3,20 +3,20 @@ def makeWritable(folderPath)
    bat(label: "Removing Read-Only flags", script: "attrib -r ${folderPath}*.* /s")
 }
 
-def copyPathFiles(sourcePath, targetPath)
+def copyPathFiles(sourcePath, targetPath, fileFilter)
 {
     try {
-        bat(label: "Copying files", script: "robocopy ${sourcePath} ${targetPath}")
+        bat(label: "Copying files", script: "robocopy ${sourcePath} ${targetPath} ${fileFilter}")
     } catch (err) {
         echo "Caught: ${err}"
         echo "Exit Code 1 means success for robocopy"
     }
 }
 
-def movePathFiles(sourcePath, targetPath)
+def movePathFiles(sourcePath, targetPath, fileFilter)
 {
     try {
-        bat(label: "Copying files", script: "robocopy ${sourcePath} ${targetPath} /mov")
+        bat(label: "Copying files", script: "robocopy ${sourcePath} ${targetPath} ${fileFilter} /mov")
     } catch (err) {
         echo "Caught: ${err}"
         echo "Exit Code 1 means success for robocopy"
